695918d65ee2865f4b1fc326b6e4a0acedfedb9c
mohamedhanif045 <hanifmohamed045@gmail.com>
2024-08-08 09:25:29 +0300

    updatedlab

--- src/components/Keypad.js
+++ src/components/Keypad.js
@@ -1,17 +1,12 @@
 import React from 'react';
 
 function Keypad() {
-  const handlePasswordChange = (event) => {
-    console.log('Entering password...');
+  const handleChange = () => {
+    console.log("Entering password...");
   };
 
   return (
-    <div>
-      <input
-        type="password"
-        onChange={handlePasswordChange}
-      />
-    </div>
+    <input type="password" onChange={handleChange} />
   );
 }
 
